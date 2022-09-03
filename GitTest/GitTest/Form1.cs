ce593578b3d28b3240d526ef078db747f8dfa29b
selamela <109020825+selamela@users.noreply.github.com>
2022-09-03 20:30:26 +0900

    First Code Commit

---
+++ GitTest/GitTest/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTest
+{
+    public partial class Form1 : Form
+    {
+        string sCheck;
+        public Form1()
+        {
+            InitializeComponent();
+            sCheck = "Version Zero";
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(sCheck);
+        }
+    }
+}
